2a5349c7e4198237c7944366d03f114dca91d21a
adm-boeckli <dboeckli@gmail.com>
2025-02-10 09:54:07 +0100

    set build and analyze in sequence

--- src/test/java/ch/dboeckli/springframeworkguru/kbe/order/services/BreweryOrderServiceTest.java
+++ src/test/java/ch/dboeckli/springframeworkguru/kbe/order/services/BreweryOrderServiceTest.java
@@ -2,9 +2,13 @@ package ch.dboeckli.springframeworkguru.kbe.order.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @DirtiesContext
@@ -12,9 +16,13 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 class BreweryOrderServiceTest {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
-        log.info("Testing Spring 6 Application...");
+        assertNotNull(applicationContext, "Application context should not be null");
+        log.info("Testing Spring 6 Application {}", applicationContext.getApplicationName());
     }
 
 }
